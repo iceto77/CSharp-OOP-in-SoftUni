122d9e8cf4b4ede592bfa9f6a7fde2e686eeff38
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-10-27 00:45:24 +0300

    Add files via upload

---
+++ Inheritance - Exercise/01.Person/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}", this.Name, this.Age));
+            return stringBuilder.ToString();
+        }
+
+    }
+}
