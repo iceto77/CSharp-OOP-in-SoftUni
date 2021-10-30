81423d4b92d5946c2d5e31dda80df74c114767f8
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-10-30 16:13:11 +0300

    Add files via upload

---
+++ Inheritance - Exercise/06.Animals/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Animal> animals = new List<Animal>();
+            while (input != "Beast!")
+            {
+                string[] info = Console.ReadLine().Split();
+                string name = info[0];
+                int age = int.Parse(info[1]);
+                string gender = info[2];
+
+                switch (input)
+                {
+                    case "Dog":
+                        Dog dog = new Dog(name, age, gender);
+                        animals.Add(dog);
+                        break;
+                    case "Frog":
+                        Frog frog = new Frog(name, age, gender);
+                        animals.Add(frog);
+                        break;
+                    case "Cat":
+                        Cat cat = new Cat(name, age, gender);
+                        animals.Add(cat);
+                        break;
+                    case "Kitten":
+                        Kitten kitten = new Kitten(name, age);
+                        animals.Add(kitten);
+                        break;
+                    case "Tomcat":
+                        Tomcat tomcat = new Tomcat(name, age);
+                        animals.Add(tomcat);
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var item in animals)
+            {
+                Console.WriteLine($"{item.GetType().Name}");
+                Console.WriteLine($"{item.Name} {item.Age} {item.Gender}");
+                Console.WriteLine($"{item.ProduceSound()}");
+            }
+        }
+    }
+}
