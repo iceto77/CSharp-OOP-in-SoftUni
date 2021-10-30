a5ba2064fa5e34e26996e0defb261bd86c5b3905
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-10-30 13:47:29 +0300

    Add files via upload

---
+++ Inheritance - Exercise/05.Restaurant/Cake.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    class Cake : Dessert
+    {
+        private const double DefaultGrams = 250;
+        private const double DefaultCalories = 1000;
+        private const decimal DefaultCakePrice = 5;
+
+        public Cake(string name) 
+            : base(name, DefaultCakePrice, DefaultGrams, DefaultCalories)
+        {
+
+        }
+    }
+}
