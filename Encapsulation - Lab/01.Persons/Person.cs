4a8259f7267b97b57c50c531c3b948960cdc3337
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-11-02 19:06:53 +0200

    Add files via upload

---
+++ Encapsulation - Lab/01.Persons/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+
+    }
+}
---
+++ Encapsulation - Lab/02.Salary/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+
+        }
+
+    }
+}
