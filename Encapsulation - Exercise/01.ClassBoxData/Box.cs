be77adc240e8ecf00ac14b08d8dfe213ffe121e8
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-11-07 11:39:05 +0200

    Add files via upload

---
+++ Encapsulation - Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double heigth)
+        {
+            Length = length;
+            Width = width;
+            Height = heigth;
+        }
+
+        public double Length
+        {
+            get => this.length;
+
+            private set
+            {
+                CheckNegativValue(value, nameof(Length));
+                this.length = value;
+            }
+        }
+
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                CheckNegativValue(value, nameof(Width));
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                CheckNegativValue(value, nameof(Height));
+                this.height = value;
+            }
+        }
+        public double SurfaceArea()
+        {
+            return (2 * length * width + 2 * length * height + 2 * width * height);
+        }
+        public double LateralSurfaceArea()
+        {
+            return (2 * length * height + 2 * width * height);
+        }
+        public double Volume()
+        {
+            return (length * height * width);
+        }
+        private void CheckNegativValue(double value, string valName)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{valName} cannot be zero or negative.");
+            }
+        }
+
+    }
+}
---
+++ Encapsulation - Exercise/01.ClassBoxData/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                var box = new Box(length, width, height);
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+        }
+    }
+}
