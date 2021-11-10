3539c6b2578d37c696c6cbd650e709f4cdfa7b17
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-11-10 08:20:36 +0200

    Add files via upload

---
+++ Polymorphism - Lab/02.Animals/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    class Dog : Animal
+    {
+        public Dog(string name, string favouriteFood)
+            : base(name, favouriteFood)
+        {
+        }
+
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() +
+                Environment.NewLine +
+                "DJAAF";
+        }
+    }
+}
