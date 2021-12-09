06af3e8c2e4b8c44008fafd6e1c52227047dbece
Hristo Hristov <85009152+iceto77@users.noreply.github.com>
2021-12-09 10:16:24 +0200

    Add files via upload

---
+++ Exam Preparation/C# OOP Exam - 10 April 2021/02. Business Logic/AquaShop/Core/Controller.cs
@@ -0,0 +1,110 @@
+﻿using AquaShop.Core.Contracts;
+using AquaShop.Models.Aquariums;
+using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations;
+using AquaShop.Repositories;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using AquaShop.Models.Fish;
+using AquaShop.Models.Fish.Contracts;
+
+namespace AquaShop.Core
+{
+    public class Controller : IController
+    {
+        private DecorationRepository decorations;
+        private List<IAquarium> aquariums;
+
+        public Controller()
+        {
+            this.decorations = new DecorationRepository();
+            this.aquariums = new List<IAquarium>();
+        }
+
+        public string AddAquarium(string aquariumType, string aquariumName)
+        {
+            switch (aquariumType)
+            {
+                case "FreshwaterAquarium":
+                    aquariums.Add(new FreshwaterAquarium(aquariumName));
+                    break;
+                case "SaltwaterAquarium":
+                    aquariums.Add(new SaltwaterAquarium(aquariumName));
+                    break;
+                default: throw new InvalidOperationException(ExceptionMessages.InvalidAquariumType);
+            }
+            return string.Format(OutputMessages.SuccessfullyAdded, aquariumType);
+        }
+        public string AddDecoration(string decorationType)
+        {
+            switch (decorationType)
+            {
+                case "Ornament":
+                    decorations.Add(new Ornament());
+                    break;
+                case "Plant":
+                    decorations.Add(new Plant());
+                    break;
+                default: throw new InvalidOperationException(ExceptionMessages.InvalidDecorationType);
+            }
+            return string.Format(OutputMessages.SuccessfullyAdded, decorationType);
+        }
+        public string InsertDecoration(string aquariumName, string decorationType)
+        {
+            var decoration = this.decorations.FindByType(decorationType);         
+            if (decoration == null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InexistentDecoration, decorationType));
+            }
+            this.aquariums.FirstOrDefault(x => x.Name == aquariumName).AddDecoration(decoration);
+            decorations.Remove(decoration);
+            return string.Format(OutputMessages.EntityAddedToAquarium, decorationType, aquariumName);
+        }
+        public string AddFish(string aquariumName, string fishType, string fishName, string fishSpecies, decimal price)
+        {
+            Fish fish;
+            switch (fishType)
+            {
+                case "FreshwaterFish":
+                    fish = new FreshwaterFish(fishName, fishSpecies, price);
+                    break;
+                case "SaltwaterFish":
+                    fish = new SaltwaterFish(fishName, fishSpecies, price);
+                    break;
+                default: throw new InvalidOperationException(ExceptionMessages.InvalidFishType);
+            }
+            if (this.aquariums.FirstOrDefault(x => x.Name == aquariumName).GetType().Name[0] == fishType[0])
+            {
+                this.aquariums.FirstOrDefault(x => x.Name == aquariumName).AddFish(fish);
+                return string.Format(OutputMessages.EntityAddedToAquarium, fishType, aquariumName);
+            }
+            return OutputMessages.UnsuitableWater;
+        }
+        public string FeedFish(string aquariumName)
+        {
+            this.aquariums.FirstOrDefault(x => x.Name == aquariumName).Feed();
+            var currentAquarium = this.aquariums.FirstOrDefault(x => x.Name == aquariumName);
+            var result = currentAquarium.Fish.Count;
+            return string.Format(OutputMessages.FishFed,result);
+        }
+        public string CalculateValue(string aquariumName)
+        {
+            var currentAquarium = this.aquariums.FirstOrDefault(x => x.Name == aquariumName);
+            var sum = currentAquarium.Fish.Sum(x => x.Price);
+            sum += currentAquarium.Decorations.Sum(x => x.Price);
+            return string.Format(OutputMessages.AquariumValue, aquariumName, $"{sum:f2}");
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in this.aquariums)
+            {
+                sb.AppendLine(item.GetInfo());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exam Preparation/C# OOP Exam - 10 April 2021/02. Business Logic/AquaShop/Models/Aquariums/Aquarium.cs
@@ -0,0 +1,87 @@
+﻿using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Models.Aquariums
+{
+    public abstract class Aquarium : IAquarium
+    {
+        private string name;
+        //private int capacity;
+        private List<IDecoration> decorations;
+        private List<IFish> fishes;
+        protected Aquarium(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.decorations = new List<IDecoration>();
+            this.fishes = new List<IFish>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAquariumName);
+                }
+                this.name = value;
+            }
+        }
+        public int Capacity { get; }
+        //o	The number of Fish аn Aquarium can have
+
+        public ICollection<IDecoration> Decorations => this.decorations;
+        public ICollection<IFish> Fish => this.fishes;
+        public int Comfort => this.decorations.Sum(x => x.Comfort);
+
+        public void AddFish(IFish fish)
+        {
+            if (fishes.Count == this.Capacity)
+            {
+                throw new InvalidOperationException(ExceptionMessages.NotEnoughCapacity);
+            }
+            fishes.Add(fish);
+        }
+        public bool RemoveFish(IFish fish)
+        {
+            return fishes.Remove(fish);
+        }
+        public void AddDecoration(IDecoration decoration)
+        {
+            decorations.Add(decoration);
+        }
+        public void Feed()
+        {
+            foreach (var item in fishes)
+            {
+                item.Eat();
+            }
+        }
+        public string GetInfo()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{this.Name} ({this.GetType().Name}):");
+            if (fishes.Count == 0)
+            {
+                sb.AppendLine($"Fish: none");
+            }
+            else
+            {
+                sb.AppendLine($"Fish: {string.Join(", ", this.fishes.Select(x => x.Name))}");
+            }
+            sb.AppendLine($"Decorations: {this.decorations.Count}");
+            sb.AppendLine($"Comfort: {this.Comfort}");
+            return sb.ToString().TrimEnd();
+        }
+
+
+    }
+}
